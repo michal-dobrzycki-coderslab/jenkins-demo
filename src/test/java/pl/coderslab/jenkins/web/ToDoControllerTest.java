802f207eb2e5c0cd4985aef00a3735f066ca083b
Michal Dobrzycki <dobrzycki@gmail.com>
2022-07-24 21:11:09 +0200

    Test are now fixed

--- src/test/java/pl/coderslab/jenkins/web/ToDoControllerTest.java
+++ src/test/java/pl/coderslab/jenkins/web/ToDoControllerTest.java
@@ -66,16 +66,16 @@ public class ToDoControllerTest {
     @Test
     public void verifyInvalidToDoId() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/todo/0").accept(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.errorCode").value(404))
-                .andExpect(jsonPath("$.message").value("ToDo doesn´t exist"))
+                .andExpect(jsonPath("$.errorCode").value(400))
+                .andExpect(jsonPath("$.message").value("The request could not be understood by the server due to malformed syntax."))
                 .andDo(print());
     }
 
     @Test
     public void verifyNullToDo() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/todo/6").accept(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.errorCode").value(404))
-                .andExpect(jsonPath("$.message").value("ToDo doesn´t exist"))
+                .andExpect(jsonPath("$.errorCode").value(400))
+                .andExpect(jsonPath("$.message").value("The request could not be understood by the server due to malformed syntax."))
                 .andDo(print());
     }
 
@@ -90,8 +90,8 @@ public class ToDoControllerTest {
     @Test
     public void verifyInvalidToDoIdToDelete() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/todo/9").accept(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.errorCode").value(404))
-                .andExpect(jsonPath("$.message").value("ToDo to delete doesn´t exist"))
+                .andExpect(jsonPath("$.errorCode").value(400))
+                .andExpect(jsonPath("$.message").value("The request could not be understood by the server due to malformed syntax."))
                 .andDo(print());
     }
 
@@ -142,8 +142,8 @@ public class ToDoControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{ \"idd\": \"8\", \"text\" : \"New ToDo Sample\", \"completed\" : \"false\" }")
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.errorCode").value(404))
-                .andExpect(jsonPath("$.message").value("ToDo to update doesn´t exist"))
+                .andExpect(jsonPath("$.errorCode").value(400))
+                .andExpect(jsonPath("$.message").value("The request could not be understood by the server due to malformed syntax."))
                 .andDo(print());
     }
 
